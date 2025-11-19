785a253fa962acc732b5286d00bda3c3b4b57d44
valpas312 <valenpalmas2005@gmail.com>
2025-11-18 23:24:30 -0300

    swagger porque ya no se ni que endpoints tengo

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('SAC REP API')
+    .setDescription(
+      'Documentación de la API para productos, equivalencias, etc.',
+    )
+    .setVersion('1.0')
+    .addTag('Productos')
+    .addTag('Mazfren')
+    .addTag('Frasle')
+    .addTag('Equivalencias')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
